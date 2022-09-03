a3fa98e2ca2de5a8997ad95eafaf6bda3b8520f4
junior <juniorbohry@gmail.com>
2022-09-03 20:55:51 -0300

    automatizando a menssagem de aviso da criação do projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -20,7 +20,7 @@ function NewProject() {
             .then(resp => resp.json())
             .then((data) => {
                 console.log(data)
-                navigate('/projects', {message: 'projeto criado com sucesso!'})
+                navigate('/projects', {state:{message: 'Projeto criado com sucesso!'}})
             })
             .catch(err => console.log(err))
     }
